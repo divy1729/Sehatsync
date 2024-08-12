6c7c2a6f338d7224e723aa81dcbfe942879e271c
Divyansh Saxena <155442934+divy1729@users.noreply.github.com>
2024-08-12 22:56:32 +0530

    Add files via upload

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,18 @@
 import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
-import {cn} from "@/lib/utils"
+
+import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/theme-provider";
 
-const FontSans = Plus_Jakarta_Sans({ subsets: ["latin"] ,
-  weight:["300","400","500","600","700"],
-  variable:"--font-sans"
-});
+const fontSans = Plus_Jakarta_Sans({ 
+  weight:['300','400','500','600','700'],
+  variable:'--font-sans',
+  subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Sehatsync",
-  description: "Simplifying patient health mangement",
+  title: "CarePulse",
+  description: "A healthcare management system",
 };
 
 export default function RootLayout({
@@ -21,13 +22,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased',FontSans.variable)}> <ThemeProvider
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+      <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            
-          >
-            {children}
-          </ThemeProvider></body>
+           
+          >{children}</ThemeProvider>
+            {children}</body>
+          
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,38 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <>
-    <h1 className="underline uppercase text-red-400">home</h1>
-    <Button>click me</Button>
-    </>
+    <div className="flex h-screen">
+      {/* to do: otp verification | passkey modal */}
+      <section className="container flex-1 flex justify-center items-center">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            height={1000}
+            width={1000}
+            alt="Patient"
+            className="mb-12 h-10 w-fit"
+          />
+
+          <PatientForm />
+          <div className="text-14-regular mt-15 flex justify-between">
+            <p className="text-dark-600 xl:text-left">© 2024 CarePulse</p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
+      </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="Patient"
+        className="max-w-[50%] h-auto object-cover"
+      />
+    </div>
   );
 }
